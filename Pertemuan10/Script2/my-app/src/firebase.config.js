a1f15b877b947cdda162106d170018ffdb5acce7
BagusSatria123 <satriabagusputra123@gmail.com>
2021-04-21 22:37:46 +0700

    Modul 10 Firebase React(1) Done

---
+++ Pertemuan10/Script2/my-app/src/firebase.config.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyAIwpRfciyEbDGMx-Jano9rhqz-tUKVr8M",
+    authDomain: "myapp-3b2f3.firebaseapp.com",
+    projectId: "myapp-3b2f3",
+    storageBucket: "myapp-3b2f3.appspot.com",
+    messagingSenderId: "16935416310",
+    appId: "1:16935416310:web:7ccff9accaa7aee97b57d7"
+  };
+ 
