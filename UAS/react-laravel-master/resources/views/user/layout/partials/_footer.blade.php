e58d38a279832bd935265c7c6161d72e6d0d90a4
BagusSatria123 <satriabagusputra123@gmail.com>
2021-06-16 20:51:05 +0700

    UAS Done

---
+++ UAS/react-laravel-master/resources/views/user/layout/partials/_footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="footer">
+<div class="d-sm-flex justify-content-center justify-content-sm-between dark">
+    <span class="text-muted text-center text-sm-left d-block d-sm-inline-block"><a href="https://github.com/BagusSatria123" target="_blank">Bagus Satria Putra</a></span>
+</div>
+</footer>
